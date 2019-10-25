bbab51e92619e380f2f648634e6dd451cbbf4f06
Cameron Stewart <cameron.stewart@weare4c.com>
2019-10-25 09:22:16 +0400

    Angular

---
+++ assets/js/default.js
@@ -0,0 +1,9 @@
+
+(function(){
+    var projectList = document.getElementsByClassName("project-list")[0];
+  	var li = document.createElement("li");
+ 		var link = document.createElement("a");
+  	li.append(link);
+  	link.append("Sample Project 2");
+  	projectList.append(li);
+})();
